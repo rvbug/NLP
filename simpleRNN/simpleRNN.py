0f89b820c5f05d1dba9f29019497a3616dc6796b
Rakesh Venkat <rakesh.venkat@gmail.com>
2023-05-11 17:19:50 +0530

    Update simpleRNN script

--- simpleRNN/simpleRNN.py
+++ simpleRNN/simpleRNN.py
@@ -1,15 +1,17 @@
-# libraries
+# Import libraries
 
 import pandas as pd
-import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from keras import Sequential
 from keras.layers import Dense, SimpleRNN
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 
 # dataset
+# you can use any dataset, but this will help us to understand 
+# the basics and how it is fed to the architecture
 simple_sent = [
     "The quick brown fox",
     "jumps over a lazy dog",
@@ -40,43 +42,71 @@ simple_sent = [
     "who knows, I am ending it here"
 ]
 
-# tokenize the data
+# call the Tokenizer class
+# helps in tokenizing the data. For more information, check the readme section
 tokenizer = Tokenizer()
+# creates unique tokens, assigns the values rathern than writing python code
 tokenizer.fit_on_texts(simple_sent)
 
-# check the output
+# shows the count of number of documents = 26
 print("sentence count -> ", tokenizer.document_count)
+
+# unique words in the vocabulary = 73
 print("\n vocab -> ", tokenizer.word_index)
 print("\n vocab length -> ", len(tokenizer.word_index))
+
+# displays the shape of the matrix = (26, 74)
 print("\n matrix shape", tokenizer.texts_to_matrix(simple_sent, mode="binary").shape)
 
-# how does the text look after assigning numbers to the word
+# with the numbers assigned to each word, how the sequence will look like
+# "The quick brown fox" will be -> [[1, 27, 28, 29], [...].. ]
 sequences = tokenizer.texts_to_sequences(simple_sent)
 print("\n showing the sequence\n ", sequences)
 
-# the sequences are not in same length so let's pad it
+# the sequences are not in same length so let's pad it to make it same length
 sequences = pad_sequences(sequences, padding="pre")
+# once padded the shape will be (26, 9) 
+# 26 sentences each with pad of 9, I have used 'pre' instead of 'post' padding
+# it is a ndarray so it can be directly fed to RNN
 print("\n after padding the sequence and shape is \n", sequences.shape, type(sequences) )
-
 print("\n", sequences)
 
 # split the data into x and y
+# since we have 9 cols, 1st 8 is for x so we can predict y
 x = sequences[:, :8]
 y = sequences[:, -1]
 
-# split the data into training and testing
+# split the data into training and testing 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
 print("\n shape is -> ", x_train.shape, x_test.shape, y_train.shape, y_test.shape)
 print("\n dimensions are -> ", x_train.ndim, x_test.ndim, y_train.ndim, y_test.ndim)
 
 
+# creating a sequential model
 model = Sequential()
 
+# 1 hidden layer with 5 neuron
+# input will be of (Batchsize, input_sequence or max_sequence_length, output_features)
+# batch size is auto calculated by TF so we need to give the other 2 
+# 8 is the max length overall and 1 is the output shape
 model.add(SimpleRNN(5, input_shape=(8, 1), name="input"))
-
+# output layer is 1 with activation as sigmoid
 model.add(Dense(1, activation='sigmoid'))
+# displays how much trainable parameters are used (weights and biases)
 model.summary()
 # tf.keras.utils.plot_model(model, show_shapes=True)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit the model
 model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+# since this is toy dataset, your accuracy will be bad 
+# but you can continue to re-train the model and tune the hyperparameter to make it work
+# or better to take a good dataset which can be trained and evaluated 
+
+
+
+
+
+
+
