418ae5876ecad0063cac4fb0680429fbc7beadc5
Rakesh Venkat <rakesh.venkat@gmail.com>
2023-05-30 17:38:35 +0530

    Update token_embeddings.py

--- transformers/token_embeddings.py
+++ transformers/token_embeddings.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 sent = [
-    "this is an example of tokenizer",
-    "tokenizer, splits sentences",
-    "another example"
+    "this is an ",
+    "tokenizer splits sentences",
+    "another example is"
 ]
 
 tokenizer = Tokenizer(lower=True)
@@ -27,14 +27,12 @@ print(result.numpy())
 result.shape # Shape will be "TensorShape([3, 3, 5])]" (batch_size, input_len, output_dim)
 
 
+
 #### output
+
 # sentence count ->  3
-
-# vocab ->  {'example': 1, 'tokenizer': 2, 'this': 3, 'is': 4, 'an': 5, 'of': 6, 'splits': 7, 'sentences': 8, 'another': 9}
-
-# vocab length ->  9
-
-# matrix shape (3, 10)
-
+# vocab ->  {'is': 1, 'this': 2, 'an': 3, 'tokenizer': 4, 'splits': 5, 'sentences': 6, 'another': 7, 'example': 8}
+# vocab length ->  8
+# matrix shape (3, 9)
 # showing the sequence
-#  [[3, 4, 5, 1, 6, 2], [2, 7, 8], [9, 1]]
+#  [[2, 1, 3], [4, 5, 6], [7, 8, 1]]
